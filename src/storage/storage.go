a9200128430804e1bc92d3825874b94e2b757081
chris <cdpaynes@gmail.com>
2017-12-24 02:14:42 -0600

    :Rename data pkg to storage

---
+++ src/storage/storage.go
@@ -0,0 +1,58 @@
+package storage
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+//CommandQueryVerifier does
+type CommandQueryVerifier interface {
+	Command(db *sql.DB, c string)
+	Query(db sql.DB, q CommandQueryVerifier) string
+	Verify(db *sql.DB, statement, assertion string) bool
+}
+
+// Command does
+func Command(db *sql.DB, command string) {
+	db.Exec(command)
+}
+
+// Query does
+func Query(db *sql.DB, q string) string {
+	var result string
+	db.QueryRow(q).Scan(&result)
+
+	return result
+}
+
+// Verify does
+func Verify(db *sql.DB, statement, assertion string) bool {
+	var result string
+	db.QueryRow(statement).Scan(&result)
+
+	return result == assertion
+}
+
+// VerifyExecs does
+func VerifyExecs(db *sql.DB, cqv CommandQueryVerifier) bool {
+	Command(db, "select ...")
+	Query(db, "select")
+
+	return Verify(db, "select", "assert")
+}
+
+type command struct {
+	// insert, update, set, drop, create
+}
+
+type query struct {
+	// select
+}
+
+// PrintExec prints SQL statements to standard output
+func PrintExec(db *sql.DB, stmnts []string) {
+	for _, s := range stmnts {
+		fmt.Printf("mysql> %s;\n", s)
+		db.Exec(s)
+	}
+}
